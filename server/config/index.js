69719cd8d15277f62c4be6474dcaab27907d6c3a
sandje00 <stella.andjelic.00@fesb.hr>
2022-03-20 12:22:17 +0100

    Add mailing service API

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,8 @@
 
 const auth = require('./auth');
 const db = require('./db');
+const mail = require('./mail');
 
 const port = process.env.PORT || 3000;
 
-module.exports = { port, db, auth };
+module.exports = { port, db, auth, mail };
