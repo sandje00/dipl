f14ba4fa6fc14018a094eb520f5d582e2e7f7eb7
sandje00 <stella.andjelic.00@fesb.hr>
2022-03-26 22:49:21 +0100

    Add base input

---
+++ client/components/common/BaseInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input
+    :value="modelValue"
+    :class="{ error }"
+    v-bind="{...$attrs, onInput: updateModelValue }"
+    class="input">
+</template>
+
+<script>
+import useModelValue from '../../composables/useModelValue';
+
+export default {
+  name: 'base-input',
+  props: {
+    label: { type: String, default: '' },
+    modelValue: { type: String, default: '' },
+    error: { type: String, default: '' }
+  },
+  setup(props, context) {
+    const { updateModelValue } = useModelValue(props, context);
+    return { updateModelValue };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.input {
+  font-size: inherit;
+  background: none;
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
