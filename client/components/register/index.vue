32a19c1872ee87079eb42bdb18f33cf05aff3a7b
sandje00 <stella.andjelic.00@fesb.hr>
2022-03-27 18:25:29 +0200

    Add validation schema

--- client/components/register/index.vue
+++ client/components/register/index.vue
@@ -5,17 +5,32 @@
         class="flex-v justify-center align-center align-items-stretch pa-l"
       >
         <base-input
+          v-model="username"
+          type="text"
+          name="username"
           label="Username"
-          error=""
+          :error="errors.username"
         ></base-input>
         <base-input
+          v-model="email"
+          name="email"
+          type="text"
           label="E-mail"
+          :error="errors.email"
         ></base-input>
         <base-input
+          v-model="password"
+          name="password"
+          type="password"
           label="Password"
+          :error="errors.password"
         ></base-input>
         <base-input
+          v-model="repeat"
+          name="repeat"
+          type="password"
           label="Repeat password"
+          :error="errors.repeat"
         ></base-input>
         <base-button class="mt-m">
           Register
@@ -26,11 +41,29 @@
 </template>
 
 <script>
+import { useForm, useField } from 'vee-validate';
 import BaseButton from '../common/BaseButton';
 import BaseInput from '../common/BaseInput';
 
 export default {
   name: 'register-view',
+  setup() {
+    const validationSchema = {
+      username: { required: true, min: 2, max: 50 },
+      email: { required: true, email: true },
+      password: { required: true, min: 8 },
+      repeat: { required: true, confirmed: '@password' }
+    };
+
+    const { errors } = useForm({ validationSchema });
+
+    const { value: username } = useField('username');
+    const { value: email } = useField('email');
+    const { value: password } = useField('password');
+    const { value: repeat } = useField('repeat');
+
+    return { errors, username, email, password, repeat }
+  },
   components: { BaseButton, BaseInput }
 }
 </script>
--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 import App from './App.vue';
 import { createApp } from 'vue';
+import { defineRules } from './utils/validation';
 import router from './router';
+
+defineRules();
 
 const app = createApp(App);
 
