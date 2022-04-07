fdb07e0401efa2043298ad0d1b49790791d791f6
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-07 17:12:33 +0200

    Add icons

---
+++ client/components/icons/IconDocs.vue
@@ -0,0 +1,15 @@
+<template>
+  <base-icon :width="36" :height="36">
+    <path d="M0 0h24v24H0V0z" fill="none"></path>
+    <path d="M8 16h8v2H8 zm0-4h8v2H8zm6-10H6c-1.1 0-2 .9-2 2v16c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm4 18H6V4h7v5h5v11z"></path>
+  </base-icon>
+</template>
+
+<script>
+import BaseIcon from '../common/BaseIcon';
+
+export default {
+  name: 'icon-docs',
+  components: { BaseIcon }
+}
+</script>
---
+++ client/components/icons/IconProjects.vue
@@ -0,0 +1,23 @@
+<template>
+  <base-icon :width="36" :height="36">
+    <g>
+      <rect fill="none" :height="24" :width="24"></rect>
+    </g>
+    <g>
+      <g>
+        <g>
+          <path d="M3,3v8h8V3H3z M9,9H5V5h4V9z M3,13v8h8v-8H3z M9,19H5v-4h4V19z M13,3v8h8V3H13z M19,9h-4V5h4V9z M13,13v8h8v-8H13z M19,19h-4v-4h4V19z"></path>
+        </g>
+      </g>
+    </g>
+  </base-icon>
+</template>
+
+<script>
+import BaseIcon from '../common/BaseIcon';
+
+export default {
+  name: 'icon-projects',
+  components: { BaseIcon }
+}
+</script>
