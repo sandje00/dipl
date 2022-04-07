16259ce587ef692de0953db346aed31826117e48
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-07 17:15:32 +0200

    Add sidebar navigation

---
+++ client/components/common/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="logo">L</span>
+</template>
+
+<script>
+export default {
+  name: 'app-logo'
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  font-size: 3rem;
+  font-weight: 700;
+}
+</style>
--- client/components/common/Sidebar.vue
+++ client/components/common/Sidebar.vue
@@ -1,12 +1,63 @@
 <template>
-  <div class="sidebar">
-
+  <div class="flex-v align-items-center sidebar">
+    <app-logo></app-logo>
+    <nav class="flex-v align-items-center mt-xl">
+      <router-link
+        :to="{ name: 'overview' }"
+        class="navlink"
+      >
+        <div class="flex-v align-items-center py-l">
+          <icon-home></icon-home>
+          <span>Overview</span>
+        </div>
+      </router-link>
+      <router-link
+        :to="{ name: 'projects' }"
+        class="navlink"
+      >
+        <div class="flex-v align-items-center py-l">
+          <icon-projects></icon-projects>
+          <span>Projects</span>
+        </div>
+      </router-link>
+      <router-link
+        :to="{ name: 'boards' }"
+        class="navlink"
+      >
+        <div class="flex-v align-items-center py-l">
+          <icon-boards></icon-boards>
+          <span>Boards</span>
+        </div>
+      </router-link>
+      <router-link
+        :to="{ name: 'docs' }"
+        class="navlink"
+      >
+        <div class="flex-v align-items-center py-l">
+          <icon-docs></icon-docs>
+          <span>Docs</span>
+        </div>
+      </router-link>
+    </nav>
   </div>
 </template>
 
 <script>
+import AppLogo from './Logo';
+import IconBoards from '../icons/IconBoards';
+import IconDocs from '../icons/IconDocs';
+import IconHome from '../icons/IconHome';
+import IconProjects from '../icons/IconProjects';
+
 export default {
-  name: 'app-sidebar'
+  name: 'app-sidebar',
+  components: {
+    AppLogo,
+    IconBoards,
+    IconDocs,
+    IconHome,
+    IconProjects
+  }
 }
 </script>
 
@@ -21,5 +72,20 @@ export default {
   background-color: var(--color-background-primary);
   border-right: 1px solid var(--color-border);
   overflow-x: hidden;
+
+  .navlink {
+    color: var(--color-text-primary);
+    // TODO Define inside _typography.scss
+    font-size: 0.8rem;
+    font-weight: 700;
+
+    &:hover {
+      color: var(--color-primary);
+
+      svg {
+        fill: var(--color-primary);
+      }
+    }
+  }
 }
 </style>
