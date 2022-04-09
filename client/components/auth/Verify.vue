eff68d463739f1e29d04d11cd8d7023637d7b114
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-09 22:28:25 +0200

    Use import alias on client side

--- client/components/auth/Verify.vue
+++ client/components/auth/Verify.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import api from '../../api/auth';
+import api from '@/api/auth';
 import BaseButton from '../common/BaseButton'
 import { ref } from 'vue';
 
--- client/components/common/BaseInput.vue
+++ client/components/common/BaseInput.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import useModelValue from '../../composables/useModelValue';
+import useModelValue from '@/composables/useModelValue';
 
 export default {
   name: 'base-input',
