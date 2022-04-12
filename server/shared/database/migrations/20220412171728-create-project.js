5d7bd0bfb933718432ee5b5668dd137c85de0b4b
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-12 22:10:09 +0200

    Add project migration

---
+++ server/shared/database/migrations/20220412171728-create-project.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('projects', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      imgUrl: {
+        type: Sequelize.TEXT,
+        field: 'img_url'
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        field: 'user_id'
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        field: 'created_at',
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        field: 'updated_at',
+        allowNull: false
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        field: 'deleted_at'
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    return queryInterface.dropTable('projects');
+  }
+};
