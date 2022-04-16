e0743883e150deb1995dd6abe2488953a40638a2
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-16 21:31:39 +0200

    Add profile container

--- client/components/layout/Header.vue
+++ client/components/layout/Header.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <div class="flex-h justify-space-between pa-s top-container">
+      <search-box></search-box>
+      <profile-container></profile-container>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProfileContainer from './ProfileContainer';
+import SearchBox from './Search';
+
 export default {
-  name: 'app-header'
+  name: 'app-header',
+  components: { ProfileContainer, SearchBox }
 }
 </script>
 
@@ -17,5 +26,10 @@ export default {
   top: 0;
   background-color: var(--color-background-primary);
   border-bottom: 1px solid var(--color-border);
+
+  .top-container {
+    // TODO Find better way instead of this workaround
+    max-width: 95%;
+  }
 }
 </style>
---
+++ client/components/layout/ProfileContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex-h align-items-center profile-container">
+    <icon-account></icon-account>
+    <span class="mr-m pl-s py-m username">{{ username }}</span>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import IconAccount from '../icons/IconAccount';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'profile-container',
+  setup() {
+    const store = useStore();
+    const username = computed(() => store.state.auth.user.username);
+
+    return { username }
+  },
+  components: { IconAccount }
+}
+</script>
+
+<style lang="scss" scoped>
+.profile-container {
+  color: var(--color-text-secondary);
+  cursor: pointer;
+
+  .username {
+    display: inline-block;
+    font-weight: 700;
+    font-size: 1.2rem;
+
+    &::after {
+      content: "";
+      display: inline-block;
+      width: 0;
+      height: 0;
+      margin-bottom: var(--spc-xxs);
+      margin-left: var(--spc-s);
+      border-left: 5px solid transparent;
+      border-right: 5px solid transparent;
+      border-top: 5px solid var(--color-text-secondary);
+    }
+  }
+}
+</style>
