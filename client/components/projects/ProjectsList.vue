e0e154fdb39f7012d919ee77b58fe7e6cf1e2b1b
sandje00 <stella.andjelic.00@fesb.hr>
2022-04-17 19:56:50 +0200

    Add projects list

---
+++ client/components/projects/ProjectsList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex-h flex-wrap pa-m projects-list">
+    <project-card
+      v-for="project in projects"
+      :key="project.id"
+      v-bind="project"
+      class="mt-l mr-xl"
+    ></project-card>
+  </div>
+</template>
+
+<script>
+import ProjectCard from './Project';
+
+export default {
+  name: 'projects-list',
+  setup() {
+    // TODO Fetch projects via API call
+    const projects = [
+      {
+        id: 1,
+        title: 'Test1',
+        description: 'Hahahahaha',
+        imgUrl: null,
+        userId: 1
+      },
+      {
+        id: 2,
+        title: 'Test2',
+        description: 'Hahahahaha',
+        imgUrl: null,
+        uderId: 1
+      },
+      {
+        id: 3,
+        title: 'Test3',
+        description: 'Hahahahaha',
+        imgUrl: null,
+        uderId: 1
+      }
+    ];
+
+    return { projects };
+  },
+  components: { ProjectCard }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- client/components/projects/index.vue
+++ client/components/projects/index.vue
@@ -3,15 +3,21 @@
     <template v-slot:page-header>
       <projects-header></projects-header>
     </template>
+    <projects-list></projects-list>
   </app-layout>
 </template>
 
 <script>
 import AppLayout from '../layout';
 import ProjectsHeader from './ProjectsHeader';
+import ProjectsList from './ProjectsList';
 
 export default {
   name: 'projects-view',
-  components: { AppLayout, ProjectsHeader }
+  components: {
+    AppLayout,
+    ProjectsHeader,
+    ProjectsList
+  }
 }
 </script>
