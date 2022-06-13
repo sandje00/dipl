7d800ed008648b87822e2491cea2033e3354bfae
sandje00 <stella.andjelic.00@fesb.hr>
2022-06-13 17:38:59 +0200

    Add OAuth2 config

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,8 @@
 const auth = require('./auth');
 const db = require('./db');
 const mail = require('./mail');
+const oauth2 = require('./oauth2');
 
 const port = process.env.PORT || 3000;
 
-module.exports = { port, db, auth, mail };
+module.exports = { port, db, auth, mail, oauth2 };
